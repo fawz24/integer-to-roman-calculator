893c610ff3c1acbf8f61a94d0dea2993638e4c64
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2022-12-26 17:12:09 +0300

    testing convertion of integers from 1 to 9

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe("Home", () => {
+  it.each([
+    [1, "I"],
+    [2, "II"],
+    [3, "III"],
+    [4, "IV"],
+    [5, "V"],
+    [6, "VI"],
+    [7, "VII"],
+    [8, "VIII"],
+    [9, "IX"],
+  ])("converts integers from 1 to 9 - (%i -> %s)", (input, output) => {
+    render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    fireEvent.change(screen.getByLabelText("Integer"), {
+      target: { value: input },
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(screen.getByTestId("roman-output")).toHaveTextContent(
+      RegExp(`^${output}$`)
+    );
+  });
+});
--- components/IntegerToRomanCalculator.tsx
+++ components/IntegerToRomanCalculator.tsx
@@ -1,11 +1,20 @@
 import { FC, useState } from "react";
 
+const lookup = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+/**
+ * Converts a given integer value to roman notation
+ * @param integer integer value to convert
+ * @returns the integer value in roman numerals form
+ */
 const romanize = (integer: number) => {
-  return `Romanized ${integer}`;
+  return lookup[integer - 1];
 };
 
 interface IntegerToRomanCalculatorProps {
+  /** the mininum accepted integer */
   min?: number;
+  /** the maxinum accepted integer */
   max?: number;
 }
 
@@ -17,24 +26,28 @@ export const IntegerToRomanCalculator: FC<IntegerToRomanCalculatorProps> = ({
   const roman = integer ? romanize(integer) : "";
   return (
     <div>
-      <input
-        type="number"
-        min={min}
-        max={max}
-        value={integer?.toString() ?? ""}
-        onChange={(e) => {
-          const num = Number(e.target.value);
-          if (e.target.value.length === 0 || num === 0) {
-            setInteger(undefined);
-            return;
-          }
-          if (num < min || num > max) {
-            return;
-          }
-          setInteger(num);
-        }}
-      />
-      <p>{roman}</p>
+      <label>
+        <span>Integer</span>
+        <input
+          type="number"
+          min={min}
+          max={max}
+          value={integer?.toString() ?? ""}
+          onChange={(e) => {
+            const num = Number(e.target.value);
+            if (e.target.value.length === 0 || num === 0) {
+              setInteger(undefined);
+              return;
+            }
+            if (num < min || num > max) {
+              return;
+            }
+            setInteger(num);
+          }}
+          name="integer"
+        />
+      </label>
+      <p data-testid="roman-output">{roman}</p>
     </div>
   );
 };
