36f291c9b1626fa3c0aab9c3167ffbba345895d0
Fawzy Abderrahman <fawzyabderrahman@gmail.com>
2022-12-26 18:39:25 +0300

    testing convertion of integers less that 100

--- components/__tests__/IntegerToRomanCalculator.test.tsx
+++ components/__tests__/IntegerToRomanCalculator.test.tsx
@@ -12,8 +12,46 @@ describe("IntegerToRomanCalculator", () => {
     [7, "VII"],
     [8, "VIII"],
     [9, "IX"],
-  ])("converts integers from 1 to 9 - (%i -> %s)", (input, output) => {
+  ])("converts integers from 1 to 9 | (%i -> %s)", (input, output) => {
     render(<IntegerToRomanCalculator min={1} max={9} />);
+
+    fireEvent.change(screen.getByLabelText("Integer"), {
+      target: { value: input },
+    });
+
+    expect(screen.getByTestId("roman-output")).toHaveTextContent(
+      RegExp(`^${output}$`)
+    );
+  });
+
+  it.each([
+    [10, "X"],
+    [20, "XX"],
+    [30, "XXX"],
+    [40, "XL"],
+    [50, "L"],
+    [60, "LX"],
+    [70, "LXX"],
+    [80, "LXXX"],
+    [90, "XC"],
+  ])("converts multiples of 10 from 10 to 90 | (%i -> %s)", (input, output) => {
+    render(<IntegerToRomanCalculator min={10} max={90} />);
+
+    fireEvent.change(screen.getByLabelText("Integer"), {
+      target: { value: input },
+    });
+
+    expect(screen.getByTestId("roman-output")).toHaveTextContent(
+      RegExp(`^${output}$`)
+    );
+  });
+
+  it.each([
+    [39, "XXXIX"],
+    [17, "XVII"],
+    [94, "XCIV"],
+  ])("converts custom integer values | (%i -> %s)", (input, output) => {
+    render(<IntegerToRomanCalculator min={1} max={1000} />);
 
     fireEvent.change(screen.getByLabelText("Integer"), {
       target: { value: input },
